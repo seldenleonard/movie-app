f75c34ea2c8416a30999b5d913deb7f66c61e06d
seldenleonard <selden222@gmail.com>
2020-10-15 00:05:18 -0700

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
     # Actor Paths
     get "/actors" => "actors#index"
     get "/actors/:id" => "actors#show"
-    # get "actors/new" => "actors#new"
-    # get "/actors/:id/edit" => "actors#edit"
     post "/actors" => "actors#create"
     patch "/actors/:id" => "actors#update"
     delete "/actors/:id" => "actors#destroy"
@@ -21,8 +19,6 @@ Rails.application.routes.draw do
     # Movie Paths
     get "/movies" => "movies#index"
     get "/movies/:id" => "movies#show"
-    # get "movies/new" => "movies#new"
-    # get "/movies/:id/edit" => "movies#edit"
     post "/movies" => "movies#create"
     patch "/movies/:id" => "movies#update"
     delete "/movies/:id" => "movies#destroy"
