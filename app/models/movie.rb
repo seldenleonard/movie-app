3823a744f167310bd1d98c0a83a8a8a03036361e
seldenleonard <selden222@gmail.com>
2020-10-15 17:47:56 -0700

    comments out validations for development testing with vue.js frontend

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ApplicationRecord
   
-  validates :title, presence: true
-  validates :year, length: {is: 4}
-  validates :plot, length: {minimum: 10}
-  validates :director, length: {maximum: 20}
-  validates :english, inclusion: {in: [true, false]}
-  validates :english, exclusion: {in: [nil]}
+  # validates :title, presence: true
+  # validates :year, length: {is: 4}
+  # validates :plot, length: {minimum: 10}
+  # validates :director, length: {maximum: 20}
+  # validates :english, inclusion: {in: [true, false]}
+  # validates :english, exclusion: {in: [nil]}
 
   has_many :actors
   has_many :movie_genres
